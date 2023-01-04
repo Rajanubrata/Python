cce58b88b19bc823e8f7eccfe281aacd808f8902
Rajanubrata <103585901+Rajanubrata@users.noreply.github.com>
2023-01-04 11:05:20 +0530

    menu

---
+++ Day15/Project/Menu.py
@@ -0,0 +1,31 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 124.23,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 207.05,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 248.46,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
