d96f8192db2e97b8815e7fbfc68739cfff4ccd6a
Rajanubrata <103585901+Rajanubrata@users.noreply.github.com>
2023-01-06 09:50:59 +0530

    Money Machine

---
+++ Day16/project/money_machine.py
@@ -0,0 +1,39 @@
+class MoneyMachine:
+    CURRENCY = "Rs. "
+
+    COIN_VALUES = {
+        "5 Rs.": 5,
+        "10 Rs.": 10,
+        "50 Rs.": 50,
+        "100 Rs.": 100
+    }
+
+    def __init__(self):
+        self.profit = 0
+        self.money_received = 0
+
+    def report(self):
+        """Prints the current profit"""
+        print(f"Money: {self.CURRENCY}{self.profit}")
+
+    def process_coins(self):
+        """Returns the total calculated from coins inserted."""
+        print("Please insert coins.")
+        for coin in self.COIN_VALUES:
+            self.money_received += int(input(f"How many {coin}?: ")) * self.COIN_VALUES[coin]
+        return self.money_received
+
+    def make_payment(self, cost):
+        """Returns True when payment is accepted, or False if insufficient."""
+        self.process_coins()
+        if self.money_received >= cost:
+            change = round(self.money_received - cost, 2)
+            print(f"Here is {self.CURRENCY}{change} in change.")
+            self.profit += cost
+            self.money_received = 0
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            self.money_received = 0
+            return False
+
