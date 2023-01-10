7f089a77e515a09c80e46ace1ce1f3aeff8305a5
Rajanubrata <103585901+Rajanubrata@users.noreply.github.com>
2023-01-10 10:55:56 +0530

    sanke & movement

---
+++ Day 20/project/snake.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+class Snake:
+
+    def __init__(self):
+        self.segment= []
+        self.create_snake()
+        self.head = self.segment[0]
+    def create_snake(self):
+        for position in STARTING_POSITION:
+            new_segment = Turtle(shape="square")
+            new_segment.penup()
+            new_segment.color("white")
+            new_segment.goto(position)
+            self.segment.append(new_segment)
+
+    def move(self):
+        for position in range(len(self.segment) - 1, 0, -1):
+            new_x = self.segment[position - 1].xcor()
+            new_y = self.segment[position - 1].ycor()
+            self.segment[position].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
