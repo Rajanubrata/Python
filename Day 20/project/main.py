2107270b51ae824ac5267248911d3e62326c2a4b
Rajanubrata <103585901+Rajanubrata@users.noreply.github.com>
2023-01-10 10:55:17 +0530

    main

---
+++ Day 20/project/main.py
@@ -0,0 +1,24 @@
+import time
+from turtle import Screen
+from snake import Snake
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+screen.tracer(0)
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+screen.exitonclick()
